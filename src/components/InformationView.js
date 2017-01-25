2c03920aca37e733820a73ade7cf6eca1479d2c1
mattk5 <mattkubota5@gmail.com>
2017-01-25 18:09:30 -0500

    working on css for the main view

--- src/components/InformationView.js
+++ src/components/InformationView.js
@@ -5,13 +5,16 @@ import React, { Component } from 'react';
 export default class InformationView extends Component {
 	render() {
 		return (
-			<li className="information">
-				<h1>Information View</h1>
-				<span className="name">{this.props.name}</span>
-				<span className="address">{this.props.address}</span>
-				<span className="phone">{this.props.phone}</span>
-				<span className="website">{this.props.website}</span>
-			</li>
+			<div className="container">
+				<li className="information">
+					<h1>Information View</h1>
+					<span className="col-xs-1 list-group-item">{this.props.name}<h1>NAME</h1></span>
+					<span className="col-xs-1 list-group-item">{this.props.picture}<h1>PICTURE</h1></span>
+					<span className="col-xs-1 list-group-item">{this.props.address}<h1>ADDRESS</h1></span>
+					<span className="col-xs-1 list-group-item">{this.props.phone}<h1>PHONE</h1></span>
+					<span className="col-xs-1 list-group-item">{this.props.website}<h1>WEBSITE</h1></span>
+				</li>
+			</div>
 		)
 	}
 }
