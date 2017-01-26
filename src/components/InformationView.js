69f04242deb33a8beb59a93f0f886bf81bb05d65
mattk5 <mattkubota5@gmail.com>
2017-01-25 19:19:27 -0500

    adjusted formatting for main container

--- src/components/InformationView.js
+++ src/components/InformationView.js
@@ -8,11 +8,11 @@ export default class InformationView extends Component {
 			<div className="container">
 				<li className="information">
 					<h1>Information View</h1>
-					<span className="col-xs-1 list-group-item">{this.props.name}<h1>NAME</h1></span>
-					<span className="col-xs-1 list-group-item">{this.props.picture}<h1>PICTURE</h1></span>
-					<span className="col-xs-1 list-group-item">{this.props.address}<h1>ADDRESS</h1></span>
-					<span className="col-xs-1 list-group-item">{this.props.phone}<h1>PHONE</h1></span>
-					<span className="col-xs-1 list-group-item">{this.props.website}<h1>WEBSITE</h1></span>
+					<span className="name col-xs-3 list-group-item">{this.props.name}Dev Bootspace</span>
+					<span className="picture col-xs-3 list-group-item">{this.props.picture}Picture</span>
+					<span className="address col-xs-3 list-group-item">{this.props.address}48 Wall St.<br/> New York, NY 10001</span>
+					<span className="phone col-xs-3 list-group-item">{this.props.phone}(808)888-8888</span>
+					<span className="website col-xs-3 list-group-item">{this.props.website}www.karz4kids.com</span>
 				</li>
 			</div>
 		)
