28375dee3152cd89d86219bd6456287d252f00c8
edwincarbajal <exaviercarbajal@gmail.com>
2017-01-27 12:55:03 -0500

    small changes

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -7,6 +7,7 @@ export default class SearchBar extends Component {
     return (
       <form>
         <input className="form-control mr-sm-2" type="text" placeholder="Search..." />
+        <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBbbMsn8y8GkRINWIvi6Y1U4xX2q3wU9IM&libraries=places"></script>
       </form>
     );
   }
