418d888f23e7ef8e520411d6013323da204fc35f
edwincarbajal <exaviercarbajal@gmail.com>
2017-01-30 17:26:37 -0500

    finally, got a map to rendergit status

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -3,11 +3,16 @@
 import React, { Component } from 'react';
 
 export default class MapView extends Component {
+	componentDidMount() {
+		let map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 8
+      });
+	}
+	
 	render() {
 		return (
-			<div className="map">
-				<p>Google map goes here</p>
-			</div>
+			<div ref="map" id="map"></div>
 		);
 	}
 }
