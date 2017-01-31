d7baa54d284be88b3acc5331507cb510cb01a5db
mattk5 <mattkubota5@gmail.com>
2017-01-31 15:08:04 -0500

    saving to pull from git

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -3,11 +3,23 @@
 import React, { Component } from 'react';
 
 export default class MapView extends Component {
+
+    
+    render: function () {
+
+        return (
+            <div className='map-gic'></div>
+            );
+    }
+
+
+module.exports = GoogleMap;	      	
+
 	render() {
 		return (
 			<div className="map">
-				<p>Google map goes here</p>
-			</div>
+				
+	    	</div>
 		);
 	}
 }
