3448bdb2d580eb086d19c5941218bca512aef3b3
edwincarbajal <exaviercarbajal@gmail.com>
2017-01-30 19:38:34 -0500

    displaying a map with hardcode lat and lng

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -4,12 +4,17 @@ import React, { Component } from 'react';
 
 export default class MapView extends Component {
 	componentDidMount() {
-		let map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8
+		const points = {lat: 40.8325080, lng: -73.9184370};
+		const map = new google.maps.Map(document.getElementById('map'), {
+        center: points,
+        zoom: 13
       });
+		const marker = new google.maps.Marker({
+			position: points,
+			map: map
+		})
 	}
-	
+
 	render() {
 		return (
 			<div ref="map" id="map"></div>
