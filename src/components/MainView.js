6869c4a0c6b19c3eff22875a150cd15ba62f43dc
mattk5 <mattkubota5@gmail.com>
2017-01-31 19:10:12 -0500

    began working on handing input

--- src/components/MainView.js
+++ src/components/MainView.js
@@ -7,6 +7,28 @@ import InformationView from './InformationView';
 
 
 export default class MainView extends Component {
+	constructor(props) {
+		super(props) 
+			this.state = {
+				place: ''
+			}
+			this.handleFormSubmit = this.handleFormSubmit.bind(this);
+		
+	}
+
+	handleFormSubmit(event) {
+		event.preventDefault()
+		$.ajax({
+
+		}).done((response) => {
+			this.setState({
+				place: reponse
+			});
+		});
+
+
+	};
+
 	// Create the search box and link it to the UI element.
 	componentDidMount() {
 		var input = document.getElementById('pac-input');
@@ -19,8 +41,11 @@ export default class MainView extends Component {
 				<div className="search-container row justify-content-md-center">
 					<div className="col-lg-5 col-md-auto">
 						<div className="row">
-							<div className="col-lg-12">
+							<div className="col-lg-12 input-group">
 								<input id="pac-input" className="controls form-control" type="text" placeholder="Search Box" />
+								<span className="input-group-btn">
+        							<button className="btn btn-default" type="button">Find</button>
+      							</span>
 							</div>
 						</div>
 					</div>
