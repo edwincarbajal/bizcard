c2b81ee1ecd5bd6725f07baa7dcfee68d127fd89
mattk5 <mattkubota5@gmail.com>
2017-02-08 14:06:08 -0500

    working with google maps api

--- src/components/MainView.js
+++ src/components/MainView.js
@@ -12,28 +12,67 @@ export default class MainView extends Component {
 			this.state = {
 				place: ''
 			}
-			this.handleFormSubmit = this.handleFormSubmit.bind(this);
+			// this.handleFormSubmit = this.handleFormSubmit.bind(this);
 		
 	}
 
-	handleFormSubmit(event) {
-		event.preventDefault()
-		$.ajax({
+	// handleFormSubmit(event) {
+	// 	$.ajax({
+	// 		method: 'GET',
+	// 		url: `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=pos&radius=1000&key=AIzaSyDEnDR2l0rNeMjl8b5guxbSQrX4rkEtgVU`,
+	// 		data: { place: event.action }
+	// 	}).done((response) => {
+	// 		this.setState({
+	// 			place: reponse
+	// 		});
+	// 	});
 
-		}).done((response) => {
-			this.setState({
-				place: reponse
-			});
-		});
 
-
-	};
+	// };
 
 	// Create the search box and link it to the UI element.
 	componentDidMount() {
 		var input = document.getElementById('pac-input');
 		var searchBox = new google.maps.places.SearchBox(input);
-	}
+		var map = new google.maps.Map(document.getElementById('map'), {
+			        center: {},
+			        zoom: 13
+			      });
+					// var marker = new google.maps.Marker({
+					// 	position: {},
+					// 	map: map
+					// });
+
+		var infoWindow = new google.maps.InfoWindow({map: map});
+
+			        // Try HTML5 geolocation.
+			        if (navigator.geolocation) {
+			          navigator.geolocation.getCurrentPosition(function(position) {
+			            var pos = {
+			              lat: position.coords.latitude,
+			              lng: position.coords.longitude
+			            };
+		var currentResults = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${pos}&radius=1000&key=AIzaSyDEnDR2l0rNeMjl8b5guxbSQrX4rkEtgVU`
+		console.log(currentResults);
+
+			            infoWindow.setPosition(pos);
+			            infoWindow.setContent('Location found.');
+			            map.setCenter(pos);
+			          }, function() {
+			            handleLocationError(true, infoWindow, map.getCenter());
+			          });
+			        } else {
+			          // Browser doesn't support Geolocation
+			          handleLocationError(false, infoWindow, map.getCenter());
+			        }
+
+							function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+								infoWindow.setPosition(pos);
+								infoWindow.setContent(browserHasGeolocation ?
+									'Error: The Geolocation service failed.' :
+									'Error: Your browser doesn\'t support geolocation.');
+								}
+	}	
 
 	render() {
 		return (
@@ -42,7 +81,7 @@ export default class MainView extends Component {
 					<div className="col-lg-5 col-md-auto">
 						<div className="row">
 							<div className="col-lg-12 input-group">
-								<input id="pac-input" className="controls form-control" type="text" placeholder="Search Box" />
+								<input id="pac-input" className="controls form-control" type="text" name="place" placeholder="Search Box" />
 								<span className="input-group-btn">
         							<button className="btn btn-default" type="button">Find</button>
       							</span>
