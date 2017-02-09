6481c0fc156b5462cd409ed093a130b253c17d31
edwincarbajal <exaviercarbajal@gmail.com>
2017-02-08 20:59:57 -0500

    controlling and passing state

--- src/components/InformationView.js
+++ src/components/InformationView.js
@@ -1,9 +1,47 @@
 'use strict';
 
 import React, { Component } from 'react';
+import $ from 'jquery';
 
 export default class InformationView extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			addressId: '',
+			information: {},
+		}
+	}
+
+	componentWillReceiveProps(nextProps) {
+		this.setState({
+			addressId: nextProps,
+		});
+	}
+
+	shouldComponentUpdate(nextProps, nextState) {
+    // return a boolean value
+    return true;
+	}
+
+	componentWillUpdate(nextProps, nextState) {
+		let id = nextProps.addressId;
+		const url = 'https://maps.googleapis.com/maps/api/place/details/json?placeid=';
+		const key = '&key=AIzaSyCjOJON60_XCD-Ulvo98ai4gthTykt-sCE';
+		$.ajax({
+			url: url+id+key,
+			type: "GET",
+			dataType: "jsonp",
+			crossDomain: true,
+			cache: false
+		}).done((response) => {
+			this.setState({
+				information: response,
+			});
+		});
+	}
+
 	render() {
+
 		return (
 			<div className="input-container">
 				<input className="form-control" type="text" placeholder="Name" readOnly />
