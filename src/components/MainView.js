64f3f01e97991a3e94f2d6fcb9d0c3e49051035b
mattk5 <mattkubota5@gmail.com>
2017-02-13 15:34:25 -0500

    display no input alert.

--- src/components/MainView.js
+++ src/components/MainView.js
@@ -63,25 +63,28 @@ export default class MainView extends Component {
 		const parsedAddress = address.replace(/, United States/, '').replace(/,/g, '').replace(/\s/g, "+");
 		const geocode = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
 		const key = '&key=AIzaSyCPV-bBnvYyA84T-Cq6xifj8hhYtPPm7mM';
-		$.ajax({
-			// call the geocode api with current address state
-			// address needs to be spaced i.e 48+Wall+St
-			url: geocode+parsedAddress+key
-
-
-			// change the state of postion object with response from api
-		}).done((response) => {
-			const addressLat = response.results[0].geometry.location.lat;
-			const addressLng = response.results[0].geometry.location.lng;
-			const addressId = response.results[0].place_id;
-			this.setState({
-				position: {
-					lat: addressLat,
-					lng: addressLng
-				},
-				addressId: addressId,
+		
+		if (!address){
+			alert('Please enter a valid address!')
+		} else {
+			$.ajax({
+				// call the geocode api with current address state
+				// address needs to be spaced i.e 48+Wall+St
+				url: geocode+parsedAddress+key
+				// change the state of postion object with response from api
+			}).done((response) => {
+				const addressLat = response.results[0].geometry.location.lat;
+				const addressLng = response.results[0].geometry.location.lng;
+				const addressId = response.results[0].place_id;
+				this.setState({
+					position: {
+						lat: addressLat,
+						lng: addressLng
+					},
+					addressId: addressId,
+				});
 			});
-		});
+		}
 		// catch any errors
 	}
 
