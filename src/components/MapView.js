773e392a8a06e119fb6f6c434a07a32dc3d6ab52
edwincarbajal <exaviercarbajal@gmail.com>
2017-09-22 16:00:58 -0400

    Prepopulate the search

--- src/components/MapView.js
+++ src/components/MapView.js
@@ -12,39 +12,11 @@ export default class MapView extends Component {
 	}
 
 	componentDidMount() {
-			var map = new google.maps.Map(document.getElementById('map'), {
-	        center: {},
-	        zoom: 13
-	      	});
-
-			var infoWindow = new google.maps.InfoWindow({map: map});
-
-	        // Try HTML5 geolocation.
-	        if (navigator.geolocation) {
-	          navigator.geolocation.getCurrentPosition(function(position) {
-	            var pos = {
-	              lat: position.coords.latitude,
-	              lng: position.coords.longitude
-	            };
-							var marker = new google.maps.Marker({
-								position: pos,
-								map: map
-							});
-	            map.setCenter(pos);
-	          }, function() {
-	            handleLocationError(true, infoWindow, map.getCenter());
-	          });
-	        } else {
-	          // Browser doesn't support Geolocation
-	          handleLocationError(false, infoWindow, map.getCenter());
-	        }
-
-					function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-						infoWindow.setPosition(pos);
-						infoWindow.setContent(browserHasGeolocation ?
-							'Error: The Geolocation service failed.' :
-							'Error: Your browser doesn\'t support geolocation.');
-						}
+		 const piedpiper = new google.maps.LatLng(37.448458, -122.139729);
+		 const map = new google.maps.Map(document.getElementById('map'), {
+	    center: piedpiper,
+	    zoom: 13
+  	});
 	}
 
 	componentWillReceievProps(nextProps) {
@@ -57,10 +29,10 @@ export default class MapView extends Component {
 	shouldComponentUpdate(nextProps, nextState) {
 		return true;
 	}
-	
+
 	componentWillUpdate(nextProps, nextState) {
 		  var myLatLng = {
-		  	lat: nextProps.position.lat, 
+		  	lat: nextProps.position.lat,
 		  	lng: nextProps.position.lng
 		  };
 
@@ -75,7 +47,7 @@ export default class MapView extends Component {
 		    title: 'Hello World!'
 		  });
 	}
-	
+
 	render() {
 		return (
 			<div ref="map" id="map"></div>
